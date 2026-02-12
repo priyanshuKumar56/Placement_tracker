a66e9e84a0fcc25e9ab70a091dfbd19637d4be51
priyanshuKumar56 <109659772+priyanshuKumar56@users.noreply.github.com>
2026-02-12 18:23:43 +0530

    A New Start

--- Client/store/provider.tsx
+++
@@ -1,9 +0,0 @@
-"use client"
-import { Provider } from "react-redux"
-import { type ReactNode, useMemo } from "react"
-import { getStore } from "./index"
-
-export default function StoreProvider({ children }: { children: ReactNode }) {
-  const store = useMemo(() => getStore(), [])
-  return <Provider store={store}>{children}</Provider>
-}
